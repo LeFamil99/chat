ed109f3dda5f27dbadd164b3f8a4cc1437a43c35
LeFamil99 <maxgaudreau9@gmail.com>
2021-01-15 17:14:43 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const debug = require("debug")("app");
+const io = require("socket.io")(http)
+const compression = require("compression");
+const helmet = require("helmet");
+
+app.use(compression());
+app.use(helmet());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+});
+
+io.on("connection", (socket) => {
+    debug("a user connected");
+    socket.on("disconnect", () => {
+        debug("user disconnected");
+    });
+
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+    });
+});
+
+http.listen(3000, ()=> {
+    debug("Listening on 3000");
+});
